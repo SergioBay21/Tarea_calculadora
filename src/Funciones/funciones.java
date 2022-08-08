4575a384872d54306ce233d934717568dbc75e44
Sergio Bay <sbaye@miumg.edu.gt>
2022-08-07 23:39:25 -0600

    Hola git

---
+++ src/Funciones/funciones.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Funciones;
+
+/**
+ *
+ * @author sergi
+ */
+public class funciones {
+    public float num_1;
+    public float num_2; 
+    public float res;
+    public String op;
+
+    public float getNum_1() {
+        return num_1;
+    }
+
+    public void setNum_1(float num_1) {
+        this.num_1 = num_1;
+    }
+
+    public float getNum_2() {
+        return num_2;
+    }
+
+    public void setNum_2(float num_2) {
+        this.num_2 = num_2;
+    }
+
+    public String getOp() {
+        return op;
+    }
+
+    public void setOp(String op) {
+        this.op = op;
+    }
+
+    public float getRes() {
+        return res;
+    }
+
+    public void setRes(float res) {
+        this.res = res;
+    }
+    
+    
+}
